c2b53067f474b9b88a4b4a34e4c81e4733e41252
wenderclaytonfilho <rednew2727@gmail.com>
2024-05-22 12:03:30 -0300

    fetch: finished UseRef

--- src/components/UseRef.tsx
+++ src/components/UseRef.tsx
@@ -1,15 +1,28 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 
 function UseRef(){
 
     const [name, setName] = useState("");
 
+    const inputRef = useRef();
+
+    const focusInput = () =>{
+        inputRef.current.focus();
+    }
 
     return(
         <div>
             <h1>UseRef</h1>
-            <input value={name} onChange={(e)=>setName(e.target.value)}/>
-            <p>Hello! My name is {name}</p>        
+            <input 
+            ref={inputRef}
+            value={name} 
+            onChange={(e)=>setName(e.target.value)}
+            />
+            <p>Hello! My name is {name}</p>
+            <button onClick={
+                focusInput
+            }>Focus Input</button>   
+            <a href="https://pt-br.react.dev/reference/react/useRef">Documentação</a>
         </div>
     )
 }
